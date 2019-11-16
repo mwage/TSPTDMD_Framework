a2018bd0555cda1aedae9d41369294cdb2fbbdbb
mwage <michael.joh.wagner@gmail.com>
2019-11-16 19:20:26 +0100

    fixed the path problems for all instances

--- src/tsp/testrunner.rs
+++ src/tsp/testrunner.rs
@@ -10,10 +10,8 @@ pub struct TestRunner {
 impl TestRunner {
     pub fn solve_instance<T: Solver>(mut solver: T, instance_name: Option<&str>) {
         if let Some(instance) = instance_name {
-            println!("{}", instance);
             TestRunner::run_instance(&mut solver, instance)  // Solve a given instance
         } else {
-            println!("All");
             TestRunner::run_all_instances(solver); // Solve all instances
         }
     }
@@ -21,11 +19,16 @@ impl TestRunner {
     fn run_all_instances<T: Solver>(mut solver: T) {
         let paths = fs::read_dir("instances").unwrap(); // Get all file paths in the instances folder
         for path in paths {
-            TestRunner::run_instance(&mut solver, path.unwrap().path().to_str().unwrap().split('\\').last().unwrap());   // Get instance name from path
+            let path_buff = path.unwrap().path();   // Get path
+            let instance_name = path_buff.to_str().unwrap().split('\\').last().unwrap();    // Get instance name from path
+            if instance_name != ".gitignore" {
+                TestRunner::run_instance(&mut solver, instance_name);   // Solve the given instance
+            }
         }
     }
 
     fn run_instance<T: Solver>(solver: &mut T, instance_name: &str) {
+        println!("Solve instance: {}", instance_name);
         let logger = Logger::new(solver, instance_name);    // Initialize logger, starts the timer
         let mut instance = InstanceParser::get_instance(instance_name); // Parse the instance from file
         solver.solve(&mut instance, &logger);   // Solve TSP instance with selected solver
